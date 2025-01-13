9268d3e81586f64655b12d748c0a996ba9eaf226
monarogg <157384787+monarogg@users.noreply.github.com>
2025-01-13 16:08:12 +0100

    3: sharing a variable, ferdig?

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var i int
+var mu sync.Mutex
+var wg sync.WaitGroup
+
+func thread_1() {
+	defer wg.Done()
+	for j := 0; j < 100000000; j++ {
+		mu.Lock()
+		i++
+		mu.Unlock()
+	}
+	fmt.Println("Thread 1 ferdig")
+}
+
+func thread_2() {
+	defer wg.Done()
+	for j := 0; j < 100000000; j++ {
+		mu.Lock()
+		i--
+		mu.Unlock()
+	}
+	fmt.Println("Thread 2 ferdig")
+}
 
 func main() {
-	fmt.Println("Hei, verden!")
+	wg.Add(2)
+	go thread_1()
+	go thread_2()
+
+	wg.Wait()
+
+	fmt.Println("Resultat:", i)
 }
